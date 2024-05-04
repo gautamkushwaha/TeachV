e9d456d5ecfafb39a574787638972e41bc40eb04
Dhilleswara <dhilleswararao19@gmail.com>
2024-05-04 13:00:51 +0530

    home page

--- frontend/src/Components/Footer/footer.jsx
+++ frontend/src/Components/Footer/footer.jsx
@@ -1,87 +1,135 @@
-import React from "react";
-import { FaLinkedinIn } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
-import { FaLinkedin } from "react-icons/fa";
-
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth' // Optional, adds smooth scrolling animation
-  });
-};
-
 const Footer = () => {
   return (
-    <footer className=" md:pl-10 py-10 z-50 bg-[#2F2B36]  text-white   mx-auto">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-12 sm:gap-x-10 md:gap-x-20 lg:gap-x-[80px] px-10 md:px-10 lg:px-5">
-          {/* Column 1 */}
-          <div className="  mb-4 md:mb-0">
-          <Link to="/" onClick={scrollToTop}><h3 className=" text-xl md:text-2xl font-semibold mb-3">Teach Your Village</h3></Link>
-            {/* <img
-              src="/footer/E-Verify_Logo-2x.png"
-              alt="company_verification"
-              className="h-8 md:h-10 mb-3 "
-            /> */}
-            <p className="mb-3">
-            Bridging the gap of education in urban and rural areas through use of technology..
-            </p>
-          </div>
-
-
-          {/* Column 3 */}
-          <div className="mb-4 md:mb-0 ">
-            <h3 className="text-xl font-semibold mb-3">Important Links</h3>
-           
-            <a href="/contact" className="mb-2 md:mb-3 inline-block transition-transform hover:-translate-y-1 text-[15px] md:text-[16px]">Home</a>
-            <br></br> 
-            <a  href="/contact" className="text-[15px] md:text-[16px] mb-2 md:mb-3 inline-block transition-transform hover:-translate-y-1">Courses</a>
-
-            <br></br>
-            <a href="/contact" className="mb-2 md:mb-3 inline-block transition-transform hover:-translate-y-1 text-[15px] md:text-[16px]"> Collaboration
-            </a>
-            <br></br>
-            <a href="/contact" className="mb-2 md:mb-3 inline-block transition-transform hover:-translate-y-1 text-[15px] md:text-[16px]"> 
-            Blog
-            </a>
-            <br></br>
-            <a href="/contact" className="mb-2 md:mb-3 inline-block transition-transform hover:-translate-y-1 text-[15px] md:text-[16px]"> About Us
-            </a>
-          </div>
-
-          {/* Column 4 */}
-          <div className="mb-4 md:mb-0  ">
-            <h3 className="text-xl font-semibold mb-3">Follow Us</h3>
-            <div className="">
-              <a
-                href="https://www.linkedin.com/in/refyn-d-47489b21/"
-                className="inline-flex mb-3 transform transition-transform hover:translate-y-[-5px] "
-                target="_blank" rel="noopener noreferrer">
-                  <FaLinkedin className="mr-2 my-auto text-[#0A66C2]"/>LinkedIn 
-                  
-              </a>
-              {/* <a href="#" className=" block mb-3 transform transition-transform hover:translate-y-[-5px]">
-                Twitter
-              </a>
-              <a href="#" className="mb-3  block transform transition-transform hover:translate-y-[-5px]">
-                Facebook
-              </a> */}
+    <footer className="self-stretch flex flex-row items-end justify-between pt-[50px] pb-9 pr-[77px] pl-[100px] box-border relative max-w-full gap-[20px] text-left text-base text-white font-poppins lg:flex-wrap lg:justify-center mq750:pl-[50px] mq750:pr-[38px] mq750:box-border mq450:pl-5 mq450:pt-8 mq450:pb-[23px] mq450:box-border">
+      <div className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] bg-black" />
+      <div className="w-[649px] flex flex-row items-start justify-center gap-[105px] min-w-[649px] max-w-full text-17xl lg:flex-1 mq750:flex-wrap mq750:gap-[52px] mq1050:min-w-full mq450:gap-[26px]">
+        <div className="flex-1 flex flex-col items-start justify-start gap-[107px] min-w-[273px] max-w-full mq450:gap-[53px]">
+          <div className="w-[400px] flex flex-col items-start justify-start gap-[10px] max-w-full">
+            <h3 className="m-0 relative text-inherit font-bold font-inherit z-[1] mq1050:text-10xl mq450:text-3xl">
+              EDPark
+            </h3>
+            <div className="self-stretch relative text-5xl font-light z-[1] mq450:text-lgi">
+              {`+1400 `}
+              <span className="capitalize">
+                codeiam community dIGIFAC first floor, AUCE, north campus
+              </span>
             </div>
           </div>
+          <div className="relative text-5xl z-[1] mq450:text-lgi">
+            © 2024 EDPark, All Rights Reserved.
+          </div>
         </div>
-        {/* Line below the footer */}
-        <div className="border-t border-gray-600 my-4 mx-5">
-          <div className="container mx-auto">
-            <div className="flex justify-between items-center text-[#0B1320]">
-              <p className="text-xs ">Refyndata @ 2024 All rights reserved</p>
-              <div className="flex space-x-4">
-                <a href="#" className=" text-xs">
-                  Terms and Conditions
-                </a>
-                <a href="#" className="text-xs">
-                  Privacy
-                </a>
+        <div className="flex flex-col items-start justify-start pt-[76px] px-0 pb-0 text-base">
+          <div className="flex flex-col items-start justify-start gap-[14px]">
+            <a
+              href="#"
+              className="relative no-underline text-white uppercase inline-block font-inter min-w-[49px] z-[1]"
+            >
+              HOME
+            </a>
+            <a
+              href="#"
+              className="relative no-underline text-white uppercase inline-block font-inter min-w-[75px] z-[1]"
+            >
+              Courses
+            </a>
+            <a
+              href="#"
+              className="relative no-underline text-white uppercase inline-block font-inter min-w-[124px] z-[1]"
+            >
+              Collabration
+            </a>
+            <a
+              href="#"
+              className="relative no-underline text-white uppercase inline-block font-inter min-w-[44px] z-[1]"
+            >
+              blog
+            </a>
+            <a
+              href="#"
+              className="h-[11px] no-underline text-white relative uppercase font-inter inline-block min-w-[81px] z-[1]"
+            >
+              about us
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-[318px] flex flex-col items-start justify-end pt-0 px-0 pb-[12.2px] box-border min-w-[318px] lg:flex-1">
+        <div className="self-stretch flex flex-col items-start justify-start gap-[29.8px]">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[10.3px] shrink-0">
+            <b className="h-[11px] relative uppercase inline-block z-[1]">
+              Join our newsletter
+            </b>
+            <div className="relative inline-block min-w-[115px] z-[1]">
+              E<span className="capitalize">mail address</span>
+            </div>
+            <div className="self-stretch flex flex-row items-start justify-start py-0 pr-0 pl-px text-sm">
+              <div className="flex-1 flex flex-col items-start justify-start gap-[3px]">
+                <div className="w-[291px] h-[50px] relative box-border z-[1] border-[1.5px] border-solid border-white" />
+                <div className="self-stretch flex flex-row items-start justify-start py-0 pr-0 pl-2.5">
+                  <div className="flex-1 flex flex-row items-start justify-start gap-[9px] mq450:flex-wrap">
+                    <div className="flex flex-col items-start justify-start pt-[7px] px-0 pb-0">
+                      <div className="w-[7px] h-[7px] relative box-border z-[1] border-[0.5px] border-solid border-white" />
+                    </div>
+                    <div className="flex-1 relative capitalize font-light inline-block min-w-[189px] z-[1]">
+                      yes subscribe me to your newsletter
+                    </div>
+                  </div>
+                </div>
               </div>
+            </div>
+            <a
+              href="#"
+              className="w-[295px] no-underline flex flex-row items-start justify-center py-[15px] px-5 box-border relative z-[1] text-black font-inter"
+            >
+              <div className="h-full w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] rounded-8xs bg-white" />
+              <b className="h-3 relative uppercase inline-block min-w-[65px] z-[1]">
+                submit
+              </b>
+            </a>
+          </div>
+          <div className="w-[295px] flex flex-row items-start justify-start py-0 px-[47px] box-border shrink-0 mq450:pl-5 mq450:pr-5 mq450:box-border">
+            <div className="flex-1 flex flex-row items-end justify-between gap-[20px] z-[1]">
+              <a href="#">
+                <img
+                  className="h-5 w-[19.4px] relative min-h-[20px]"
+                  loading="lazy"
+                  alt=""
+                  src="/vector.svg"
+                />
+              </a>
+              <a href="#">
+                <img
+                  className="self-stretch w-[10.1px] relative max-h-full min-h-[20px]"
+                  loading="lazy"
+                  alt=""
+                  src="/vector-1.svg"
+                />
+              </a>
+              <a href="#">
+                <img
+                  className="h-[19px] w-[22.1px] relative"
+                  loading="lazy"
+                  alt=""
+                  src="/vector-2.svg"
+                />
+              </a>
+              <a href="#">
+                <img
+                  className="h-[18.1px] w-[23.5px] relative"
+                  loading="lazy"
+                  alt=""
+                  src="/youtube.svg"
+                />
+              </a>
+              <a href="">
+                <img
+                  className="w-[14.8px] h-[17.2px] relative"
+                  loading="lazy"
+                  alt=""
+                  src="/-icon-linkedin.svg"
+                />
+              </a>
             </div>
           </div>
         </div>
@@ -91,13 +139,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
-{/* 
-<Link to="/" >Home</Link>
-<Link to="/about" ></Link>
-<Link to="/services" ></Link>
-<Link to="/careers" ></Link>
-<Link to="/clients" ></Link>
-<Link to="/contact" ></Link> 
-*/}
--- frontend/src/Components/Navbar/navbar.jsx
+++ frontend/src/Components/Navbar/navbar.jsx
@@ -11,46 +11,125 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-black text-white font-bold ">
-      <div className="container mx-auto px-4 py-3 flex   items-center justify-between">
-        <Link to="/" onClick={closeMenu} className="flex flex-col md: items-center">
-          <img src="/navbar/center.svg" alt="Teach_Your_village" className="h-8 md:h-10 lg:h-12 md:display-inline" />
-          <div className={`md:block ${isOpen ? 'hidden' : 'block'}  md:hidden `}>
+    // <nav className="bg-black text-white font-bold ">
+    //   <div className="container mx-auto px-4 py-3 flex   items-center justify-between">
+    //     <Link to="/" onClick={closeMenu} className="flex flex-col md: items-center">
+    //       <img src="/navbar/center.svg" alt="Teach_Your_village" className="h-8 md:h-10 lg:h-12 md:display-inline" />
+    //       <div className={`md:block ${isOpen ? 'hidden' : 'block'}  md:hidden `}>
 
 
-          </div>
+    //       </div>
           
-        </Link>
+    //     </Link>
       
 
-        <div className="hidden md:flex md:space-x-1 lg:space-x-4 ">
-          <Link to="/" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md ">Home</Link>
-          <Link to="/about" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">About us</Link>
-          <Link to="/services" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">Courses</Link>
-          <Link to="/careers" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">Collaboration</Link>
-          <Link to="/clients" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">Blogs</Link>
-          <button className="bg-orange-500 text-white rounded-full px-4 py-2">    <Link to="/contact" className=" px-2 lg:px-3 py-2 rounded-md">SIGN UP </Link></button>
+    //     <div className="hidden md:flex md:space-x-1 lg:space-x-4 ">
+    //       <Link to="/" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md ">Home</Link>
+    //       <Link to="/about" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">About us</Link>
+    //       <Link to="/services" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">Courses</Link>
+    //       <Link to="/careers" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">Collaboration</Link>
+    //       <Link to="/clients" className="hover:bg-gray-200 px-2 lg:px-3 py-2 rounded-md">Blogs</Link>
+    //       <button className="bg-orange-500 text-white rounded-full px-4 py-2">    <Link to="/contact" className=" px-2 lg:px-3 py-2 rounded-md">SIGN UP </Link></button>
+    //     </div>
+    //     {/* Hamburger icon */}
+    //     <button onClick={() => setIsOpen(!isOpen)} className="md:hidden focus:outline-none">
+    //       <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    //         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+    //       </svg>
+    //     </button>
+    //   </div>
+    //   {/* Mobile menu */}
+    //   <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
+    //     <hr></hr>
+    //     <div className='mt-3'>
+    //     <Link to="/" onClick={closeMenu} className="hover:bg-gray-200 px-5 py-1 rounded-md">Home</Link>
+    //     <Link to="/about" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">About us</Link>
+    //     <Link to="/services" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">Services</Link>
+    //     <Link to="/careers" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">Careers</Link>
+    //     <Link to="/clients" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">Clients</Link>
+    //     <Link to="/contact" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 pb-3 rounded-md">Contact us</Link>
+    // </div>
+    //   </div>
+    // </nav>
+
+    <header className="self-stretch h-[100px] sticky top-[0] z-[99] max-w-full text-left text-17xl text-white font-space-grotesk bg-black">
+        <div className="absolute top-[0px] left-[0px] w-full overflow-y-auto flex flex-row items-start justify-center py-[29px] px-5 box-border max-w-full h-full z-[2]">
+          <div className="w-[1240px] overflow-hidden shrink-0 flex flex-row items-start justify-between py-0 pr-[97px] pl-0 box-border gap-[20px] max-w-full lg:pr-12 lg:box-border mq750:pr-6 mq750:box-border">
+            <div className="flex flex-col items-start justify-start pt-2 px-0 pb-0">
+              <h3 className="m-0 h-[25px] relative text-inherit font-bold font-inherit inline-block min-w-[120px] whitespace-nowrap">
+                EdPark
+              </h3>
+            </div>
+
+            <nav className="m-0 flex flex-row items-start justify-start max-w-full text-left text-lg text-white font-space-grotesk">
+              <div className="flex flex-row items-start justify-start pt-[15px] pb-[3px] pr-3 pl-[13px]">
+                <a
+                  href="#"
+                  className="relative no-underline text-white capitalize inline-block min-w-[50px]"
+                >
+                  Home
+                </a>
+              </div>
+              <div className="flex flex-row items-start justify-start py-[9px] pr-3 pl-[13px]">
+                <a
+                  href="#"
+                  className="flex flex-row no-underline text-white items-start justify-start"
+                >
+                  <div className="flex flex-col items-start justify-start pt-[5px] px-0 pb-0">
+                    <b className="h-[13px] capitalize relative inline-block min-w-[70px]">
+                      Courses
+                    </b>
+                  </div>
+                  <img
+                    className="h-6 w-6 relative overflow-hidden shrink-0 z-[2] ml-[-2.5px] pt-[8px]"
+                    alt=""
+                    src="/arrow-drop-down.svg"
+                  />
+                </a>
+              </div>
+              <div className="flex flex-row items-start justify-start py-[9px] px-[18px]">
+                <a
+                  href="#"
+                  className="flex flex-row no-underline text-white items-start justify-start"
+                >
+                  <div className="h-6 flex flex-col capitalize items-start justify-start pt-[5px] px-0 pb-0 box-border">
+                    <b className="relative shrink-0">Collaboration</b>
+                  </div>
+                  <img
+                    className="h-6 w-6 relative overflow-hidden shrink-0 min-h-[24px] pt-[8px] z-[1] ml-[-1.9px]"
+                    alt=""
+                    src="/arrow-drop-down.svg"
+                  />
+                </a>
+              </div>
+              <div className="flex flex-row items-start justify-start pt-3.5 pb-[9px] pr-4 pl-[17px]">
+                <a
+                  href="#"
+                  className="relative capitalize no-underline text-white inline-block min-w-[46px]"
+                >
+                  blog
+                </a>
+              </div>
+              <div className="flex flex-row items-start justify-start pt-[15px] px-[19px] pb-[3px]">
+                <a
+                  href="#"
+                  className="relative no-underline text-white capitalize inline-block min-w-[86px] whitespace-nowrap"
+                >
+                  about us
+                </a>
+              </div>
+              <a
+                href="#"
+                className="flex flex-row no-underline text-white items-start justify-start m-[10px] bg-orangered rounded-full"
+              >
+                <b className="relative uppercase inline-block min-w-[86px] pl-[15px] py-[5px] whitespace-nowrap">
+                  SIGN UP
+                </b>
+              </a>
+            </nav>
+          </div>
         </div>
-        {/* Hamburger icon */}
-        <button onClick={() => setIsOpen(!isOpen)} className="md:hidden focus:outline-none">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-          </svg>
-        </button>
-      </div>
-      {/* Mobile menu */}
-      <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
-        <hr></hr>
-        <div className='mt-3'>
-        <Link to="/" onClick={closeMenu} className="hover:bg-gray-200 px-5 py-1 rounded-md">Home</Link>
-        <Link to="/about" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">About us</Link>
-        <Link to="/services" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">Services</Link>
-        <Link to="/careers" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">Careers</Link>
-        <Link to="/clients" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 rounded-md">Clients</Link>
-        <Link to="/contact" onClick={closeMenu} className="hover:bg-gray-200 block px-5 py-1 pb-3 rounded-md">Contact us</Link>
-    </div>
-      </div>
-    </nav>
+      </header>
   );
 }
 
