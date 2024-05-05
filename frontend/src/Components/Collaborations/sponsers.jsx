fd6f639a0805063110e51dfbcde1d975cead4d0f
Dhilleswara <dhilleswararao19@gmail.com>
2024-05-05 10:56:15 +0530

    all pages basic

---
+++ frontend/src/Components/Collaborations/sponsers.jsx
@@ -0,0 +1,151 @@
+const COLLABRATIONWITHCOLLEGES = () => {
+  return (
+    <div className="w-full relative bg-white h-[982px] overflow-hidden text-left text-lg text-white font-poppins">
+      <img
+        className="absolute top-[0px] left-[0px] w-[1440px] h-[100px]"
+        alt=""
+        src="/rectangle-35.svg"
+      />
+      <div className="absolute top-[29px] left-[100px] w-[1240px] h-[42px] overflow-hidden font-space-grotesk">
+        <b className="absolute top-[8px] left-[0px] text-17xl">EdPark</b>
+        <div className="absolute top-[0px] left-[1141px] w-[99px] h-[42px] overflow-hidden text-base">
+          <div className="absolute top-[0px] left-[2px] bg-orangered box-border w-[95px] h-[42px] border-[1px] border-solid border-black" />
+          <b className="absolute top-[16px] left-[20px] uppercase">sign up</b>
+        </div>
+        <div className="absolute top-[0px] left-[940px] w-[79px] h-[42px]">
+          <b className="absolute top-[10px] left-[17px] uppercase">blog</b>
+        </div>
+        <div className="absolute top-[0px] left-[545px] w-[75px] h-[42px]">
+          <b className="absolute top-[15px] left-[13px] uppercase">HOME</b>
+        </div>
+        <div className="absolute top-[0px] left-[1019px] w-[124px] h-[42px]">
+          <b className="absolute top-[15px] right-[19px] uppercase">about us</b>
+        </div>
+        <div className="absolute top-[0px] left-[620px] w-[124px] h-[42px]">
+          <div className="absolute top-[calc(50%_-_12px)] left-[13px] w-[99px] h-6">
+            <b className="absolute top-[calc(50%_-_7px)] left-[0px] uppercase">
+              Courses
+            </b>
+            <img
+              className="absolute top-[calc(50%_-_12px)] left-[75px] w-6 h-6 overflow-hidden"
+              alt=""
+              src="/arrow-drop-down.svg"
+            />
+          </div>
+        </div>
+        <div className="absolute top-[calc(50%_-_21px)] left-[744px] w-[196px] h-[42px]">
+          <div className="absolute top-[9px] left-[18px] w-40 h-6">
+            <b className="absolute top-[5px] left-[0px] uppercase">
+              Collaboration
+            </b>
+            <img
+              className="absolute top-[0px] left-[136px] w-6 h-6 overflow-hidden"
+              alt=""
+              src="/arrow-drop-down.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[576px] left-[0px] w-[1440px] h-[406px] text-base">
+        <div className="absolute top-[0px] left-[0px] bg-gray-200 w-[1440px] h-[406px]" />
+        <b className="absolute top-[50px] left-[100px] text-17xl">EDPark</b>
+        <div className="absolute top-[266px] left-[1045px] w-[295px] h-[42px] text-black font-inter">
+          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-white w-[295px] h-[42px]" />
+          <b className="absolute top-[15px] left-[115px] uppercase inline-block w-[65px]">
+            submit
+          </b>
+        </div>
+        <div className="absolute h-[4.93%] w-[13.96%] top-[83.2%] right-[10.21%] bottom-[11.87%] left-[75.83%]">
+          <img
+            className="absolute h-[95%] w-[11%] top-[5%] right-[46.82%] bottom-[0%] left-[42.19%] max-w-full overflow-hidden max-h-full"
+            alt=""
+            src="/vector3.svg"
+          />
+          <img
+            className="absolute h-full w-[5.02%] top-[0%] right-[71.59%] bottom-[0%] left-[23.38%] max-w-full overflow-hidden max-h-full"
+            alt=""
+            src="/vector4.svg"
+          />
+          <img
+            className="absolute h-[90.5%] w-[11.69%] top-[9.5%] right-[21.34%] bottom-[0%] left-[66.97%] max-w-full overflow-hidden max-h-full"
+            alt=""
+            src="/youtube1.svg"
+          />
+          <img
+            className="absolute h-full w-[9.65%] top-[0%] right-[90.35%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
+            alt=""
+            src="/vector5.svg"
+          />
+          <img
+            className="absolute h-[86%] w-[7.36%] top-[9%] right-[0%] bottom-[5%] left-[92.64%] max-w-full overflow-hidden max-h-full"
+            alt=""
+            src="/-icon-linkedin1.svg"
+          />
+        </div>
+        <b className="absolute top-[126px] left-[625px] uppercase font-inter">
+          HOME
+        </b>
+        <b className="absolute top-[126px] left-[1045px] uppercase">
+          Join our newsletter
+        </b>
+        <b className="absolute top-[162px] left-[625px] uppercase">Courses</b>
+        <b className="absolute top-[198px] left-[625px] uppercase">
+          Collabration
+        </b>
+        <b className="absolute top-[234px] left-[625px] uppercase font-inter">
+          blog
+        </b>
+        <b className="absolute top-[270px] left-[625px] uppercase">about us</b>
+        <b className="absolute top-[270px] left-[625px] uppercase">about us</b>
+        <div className="absolute top-[182px] left-[1046px] box-border w-[291px] h-[50px] border-[1.5px] border-solid border-white" />
+        <div className="absolute top-[242px] left-[1056px] box-border w-[7px] h-[7px] border-[0.5px] border-solid border-white" />
+        <div className="absolute top-[235px] left-[1072px] text-sm capitalize font-light flex items-center w-[291px]">
+          yes subscribe me to your newsletter
+        </div>
+        <div className="absolute top-[333px] left-[100px] text-5xl flex items-center w-[420px] h-[23px]">
+          © 2024 EDPark, All Rights Reserved.
+        </div>
+        <div className="absolute top-[115px] left-[100px] text-5xl font-light flex items-center w-[400px]">
+          <span className="w-full">
+            {`+1400 `}
+            <span className="capitalize">
+              codeiam community dIGIFAC first floor, AUCE, north campus
+            </span>
+          </span>
+        </div>
+        <div className="absolute top-[148px] left-[1045px] flex items-center w-[115px]">
+          <span className="w-full">
+            E<span className="capitalize">mail address</span>
+          </span>
+        </div>
+      </div>
+      <div className="absolute top-[276px] left-[calc(50%_-_620px)] w-[1240px] h-[200px]">
+        <img
+          className="absolute top-[0px] left-[0px] w-[200px] h-[200px] object-cover"
+          alt=""
+          src="/rectangle-36@2x.png"
+        />
+        <img
+          className="absolute top-[0px] left-[347px] w-[200px] h-[200px] object-cover"
+          alt=""
+          src="/rectangle-38@2x.png"
+        />
+        <img
+          className="absolute top-[0px] left-[694px] w-[200px] h-[200px] object-cover"
+          alt=""
+          src="/rectangle-43@2x.png"
+        />
+        <img
+          className="absolute top-[0px] left-[1041px] w-[200px] h-[200px] object-cover"
+          alt=""
+          src="/rectangle-37@2x.png"
+        />
+      </div>
+      <div className="absolute top-[200px] left-[100px] text-17xl uppercase font-extrabold text-black">
+        OUR SPONSORS
+      </div>
+    </div>
+  );
+};
+
+export default COLLABRATIONWITHCOLLEGES;
